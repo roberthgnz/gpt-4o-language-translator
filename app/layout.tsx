2a526dfd978d733749c06a44ced9a50ab3cc4d4c
Roberth González <reliutg@gmail.com>
2024-05-14 22:15:57 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 import "./globals.css"
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   )
