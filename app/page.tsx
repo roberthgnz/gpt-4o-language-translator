59c965b7488d8efde5f3301791a0aa563017ddca
Roberth González <reliutg@gmail.com>
2024-05-14 23:49:43 +0200

    chore: Refactor Home component for improved layout and code organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 "use client"
 
 import { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
-import { Globe } from "lucide-react";
 import { TranslatorTypeTabs } from "@/components/translator-type-tabs";
 import { TabsContent } from "@/components/ui/tabs";
 import { TextTranslator } from "@/components/text-translator";
-
 
 export default function Home() {
   const [activeTab] = useState("text");
@@ -30,28 +21,20 @@ export default function Home() {
   ];
 
   return (
-    <div className="container mx-auto p-4">
-      <Card className="w-full max-w-2xl mx-auto">
-        <CardHeader className="flex items-center">
-          <Globe className="mr-2 h-6 w-6" />
-          <CardTitle>Language Translator</CardTitle>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <TranslatorTypeTabs defaultValue={activeTab} tabsList={tabsList}>
-            <TabsContent value="text">
-              <TextTranslator />
-            </TabsContent>
-            <TabsContent value="image">
-              Work in progress...
-            </TabsContent>
-          </TranslatorTypeTabs>
-        </CardContent>
-        <CardFooter>
-          <p className="w-full text-center text-sm text-gray-500 dark:text-gray-400">
-            Powered by OpenAI
-          </p>
-        </CardFooter>
-      </Card>
+    <div className="container mx-auto my-4">
+      <TranslatorTypeTabs defaultValue={activeTab} tabsList={tabsList}>
+        <div className="p-4 rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-2xl mx-auto">
+          <TabsContent className="mt-0" value="text">
+            <TextTranslator />
+          </TabsContent>
+          <TabsContent className="mt-0" value="image">
+            Work in progress...
+          </TabsContent>
+        </div>
+        <p className="w-full text-center text-sm text-gray-500 dark:text-gray-400">
+          Powered by OpenAI
+        </p>
+      </TranslatorTypeTabs>
     </div>
   );
 }
